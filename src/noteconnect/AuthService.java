c24c00e34a3dcf52151332e86942f5b439a55b5e
xc528 <xc2344@nyu.edu>
2025-05-08 13:15:40 -0400

    implemented user related classes

--- src/noteconnect/AuthService.java
+++ src/noteconnect/AuthService.java
@@ -1,1 +1,44 @@
 package noteconnect;
+
+import java.util.*;
+import java.io.*;
+
+
+public class AuthService {
+	private UserRepository userRepo;
+	public User currentUser;
+
+	public AuthService() {
+		userRepo = new UserRepository();
+	}
+	
+	public User register(String email, String password) {
+		User u = new User(email, password, (userRepo.totalnum+1));
+		userRepo.save(u);
+		System.out.println(userRepo.totalnum);
+		return u;
+	}
+	
+	public boolean login(String email, String password) {
+		User cur = userRepo.findByEmail(email);
+		if(cur != null) {
+			if(cur.validatePassword(cur.hashPassword(password))) {
+				currentUser = cur;
+				return true;
+			}
+			
+			else {
+				System.out.print("wrong password");
+				return false;
+			}
+			
+		}
+		else
+			return false;
+	}
+	
+	public void logout() {
+		currentUser = null;
+	}
+
+}
--- src/noteconnect/User.java
+++ src/noteconnect/User.java
@@ -1,1 +1,43 @@
 package noteconnect;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+
+public class User {
+	//static int totalnum;
+	int ID;
+	String email;
+	String passwordHash;
+
+	public User(String email, String password, int ID) {
+		this.email = email;
+		setPassword(password);
+		//totalnum += 1;
+		this.ID = ID;
+	}
+	public boolean validatePassword(String pw){
+		
+		return passwordHash.equals(hashPassword(pw));
+	}
+	public void setPassword(String pw) {
+		passwordHash = hashPassword(pw);
+	}
+	
+	public String hashPassword(String password) {
+        try {
+            // Get an instance of SHA-512
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            // Compute the hash
+            byte[] hashedBytes = md.digest(password.getBytes());
+            // Encode the hash to a readable string (Base64)
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error: SHA-512 algorithm not available.", e);
+        }
+    }
+
+}
