54811ce8b0a9457f5a00c1f6d78e566c875a84f8
jwhit0 <jjw9064@nyu.edu>
2025-05-10 12:24:02 -0400

    modified note classes

--- src/noteconnect/NoteRepository.java
+++ src/noteconnect/NoteRepository.java
@@ -1,14 +1,41 @@
 package noteconnect;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import javax.swing.DefaultListModel;
 
 public class NoteRepository {
     private Map<String, Note> notes = new HashMap<>();
+    private final FileManager fileManager;
 
-    public void save(Note note) {
-        notes.put(note.getId(), note);
+    public NoteRepository(FileManager fileManager) throws IOException {
+        this.fileManager = fileManager;
+        
+        DefaultListModel<Note> model = fileManager.read();
+        for (int i = 0; i < model.size(); i++) {
+            Note n = model.getElementAt(i);
+            notes.put(n.getId(), n);
+        }
     }
+
+    public void save(Note note) throws IOException {
+        notes.put(note.getId(), note);
+        DefaultListModel<Note> model = new DefaultListModel<>();
+        notes.values().forEach(model::addElement);
+        fileManager.save(model);
+    }
+
+    public void delete(Note note) throws IOException {
+        notes.remove(note.getId());
+        DefaultListModel<Note> model = new DefaultListModel<>();
+        notes.values().forEach(model::addElement);
+        fileManager.save(model);
+    }
+
+    /*public void save(Note note) {
+        notes.put(note.getId(), note);
+    }*/
 
     public Note findById(String id) {
         return notes.get(id);
@@ -30,7 +57,7 @@ public class NoteRepository {
     // public List<Note> findSharedWith(User user) {
     // }
 
-    public void delete(Note note) {
+    /*public void delete(Note note) {
         notes.remove(note.getId());
-    }
+    }*/
 }
